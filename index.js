fe24fe49bd3c139a199594d8a917f2988b95d4ee
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-15 17:07:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@
 
 
 function checkAge(age) {
-
+return age>18;
   }
 
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
   function min(a, b) {
-   
+   return Math.min(a,b);
   }
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
   function pow(x, n) {
-    
+    return x**n;
   }
